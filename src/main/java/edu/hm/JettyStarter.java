25bd582d58dc33b1951a9cd5f899daf11eaa75ad
basti <basti@basti-VirtualBox>
2017-04-23 21:58:43 +0200

    not sure how the rest api works

--- src/main/java/edu/hm/JettyStarter.java
+++ src/main/java/edu/hm/JettyStarter.java
@@ -1,7 +1,8 @@
 package edu.hm;
 
-import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.webapp.*;
+import org.eclipse.jetty.server.Server;
+
+import edu.hm.logic.MediaResource;
 
 /**
  * Start the application without an AppServer like tomcat.
@@ -16,7 +17,8 @@ public class JettyStarter {
 
     public static void main(String... args) throws Exception {
         Server jetty = new Server(PORT);
-        jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
+        jetty.setHandler(new MediaResource());
+        //jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
         jetty.start();
         System.out.println("Jetty listening on port " + PORT);
         jetty.join();
