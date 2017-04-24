1c0ed9c394a65b0af3aebbcc2f9e1952a69e3bf8
ifw12013 <ifw12013@RA03.pn.cs.hm.edu>
2017-04-24 14:58:08 +0200

    added Resources

--- src/main/java/edu/hm/api/MediaResource.java
+++ src/main/java/edu/hm/api/MediaResource.java
@@ -3,30 +3,43 @@ package edu.hm.api;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import edu.hm.logic.MediaServiceResult;
-
-public class MediaResource{
+@Path("media")
+public class MediaResource {
 	@POST
-	@Path("/media/books")
+	@Path("books")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String createNewBook(){
+	public String createNewBook() {
 		System.out.println("test");
 		String returnString = "";
-		return returnString;
+		return null;
 	}
-	
 	
 	@GET
-	@Path("/media/books/{isbn}")
-	public MediaServiceResult getbooks(){
-		return MediaServiceResult.OK;
+	@Path("books/{isbn}")
+	public String getBooks(@PathParam("isbn") String name) {
+		// return MediaServiceResult.OK;
+		return "hello world";
 	}
 	
-
+	@GET
+	@Path("books")
+	public String listBooks(){
+		return "books";
+	}
 	
+	@PUT
+	@Path("books/{isbn}")
+	public String modifyBook(){return "lol";}
+	
+	
+	
+	
+
 }
